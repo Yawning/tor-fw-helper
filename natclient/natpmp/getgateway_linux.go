02b3042e6d963fb134ad950e8a502bcc8225ded7
Yawning Angel <yawning@schwanenlied.me>
2014-10-24 20:48:11 +0000

    Improve the Linux implementation of getGateway().
    Changes:
     * Properly return errors if the family is not AF_INET when looking at
       the returned table entries.
     * Manually parse RtMsg, and return IP + Masks for the
       source/destination networks, instead of just IPs.
    
    The old code worked, but was kind of nasty, this should be much more
    readable and serve as an example on how to do this correctly for future
    generations.

--- natclient/natpmp/getgateway_linux.go
+++ natclient/natpmp/getgateway_linux.go
@@ -11,47 +11,65 @@ import (
 	"syscall"
 )
 
-type tableEntry struct {
-	srcAddr net.IP
-	dstAddr net.IP
-	gwAddr  net.IP
+type routeEntry struct {
+	syscall.RtMsg
+	SrcNet net.IPNet
+	DstNet net.IPNet
+	GwAddr net.IP
 }
 
-func routeToTableEntry(attrs []syscall.NetlinkRouteAttr) (*tableEntry, error) {
-	var e tableEntry
-	for _, attr := range attrs {
-		// The routing attributes are Key/Length/Value, so cut the value
-		// down to the length before figuring out what it is.  Note: IPv4
-		// addresses are actually shorter than Attr.Len, but the runtime's
-		// attribute parser doesn't let me easily get at RtMsg.  This will need
-		// to be handled if we ever care about IPv6 (so basically never).
-		v := attr.Value[:attr.Attr.Len]
-		switch attr.Attr.Type {
+func parseRTMNewRoute(m *syscall.NetlinkMessage) (*routeEntry, error) {
+	// The runtime doesn't expose RtMsg, which is needed to get the length of
+	// various fields, so ensure that there is enough data present, and decode
+	// the header by hand.
+	if len(m.Data) < syscall.SizeofRtMsg {
+		// Trunncated message.
+		return nil, syscall.EINVAL
+	}
+	e := &routeEntry{}
+	e.Family = m.Data[0]
+	e.Dst_len = m.Data[1]
+	e.Src_len = m.Data[2]
+	e.Tos = m.Data[3]
+	e.Table = m.Data[4]
+	e.Protocol = m.Data[5]
+	e.Scope = m.Data[6]
+	e.Type = m.Data[7]
+	e.Flags = uint32(m.Data[8]<<24) | uint32(m.Data[9]<<16) | uint32(m.Data[10]<<8) | uint32(m.Data[11])
+	if e.Family != syscall.AF_INET {
+		return nil, syscall.EAFNOSUPPORT
+	}
+
+	// Parse all the attributes associated with this routing table entry.
+	attrs, err := syscall.ParseNetlinkRouteAttr(m)
+	if err != nil {
+		return nil, err
+	}
+	for _, a := range attrs {
+		// Attr.Len is full of lies (not neccecarily 32 bits for AF_INET
+		// addresses), but if the kernel is returning messages with the
+		// incorrect address length when the family in the header is
+		// AF_INET, there are bigger problems.
+		v := a.Value[:a.Attr.Len]
+		switch a.Attr.Type {
 		case syscall.RTA_DST:
 			// Route destination address.
-			if len(v) < net.IPv4len {
-				return nil, syscall.ERANGE
-			}
-			e.dstAddr = net.IPv4(v[0], v[1], v[2], v[3])
+			e.DstNet.Mask = net.CIDRMask(int(e.Dst_len), 32)
+			e.DstNet.IP = net.IPv4(v[0], v[1], v[2], v[3])
 		case syscall.RTA_SRC:
 			// Route source address.
-			if len(v) < net.IPv4len {
-				return nil, syscall.ERANGE
-			}
-			e.srcAddr = net.IPv4(v[0], v[1], v[2], v[3])
+			e.SrcNet.Mask = net.CIDRMask(int(e.Src_len), 32)
+			e.SrcNet.IP = net.IPv4(v[0], v[1], v[2], v[3])
 		case syscall.RTA_GATEWAY:
 			// The gateway of the route.
-			if len(v) < net.IPv4len {
-				return nil, syscall.ERANGE
-			}
-			e.gwAddr = net.IPv4(v[0], v[1], v[2], v[3])
+			e.GwAddr = net.IPv4(v[0], v[1], v[2], v[3])
 		default:
-			// Ignore RTA_<bleah> when it doesn't help us get what we want, not
-			// an error since the attributes include things like the
+			// Ignore RTA_<bleah> when it doesn't help us get what we want,
+			// not an error since the attributes include things like the
 			// interface/priority etc.
 		}
 	}
-	return &e, nil
+	return e, nil
 }
 
 func getGateway() (net.IP, error) {
@@ -66,22 +84,18 @@ func getGateway() (net.IP, error) {
 	if err != nil {
 		return nil, err
 	}
-	rtTable := make([]*tableEntry, 0, len(msgs))
+	rtTable := make([]*routeEntry, 0, len(msgs))
 msgLoop:
 	for _, msg := range msgs {
 		switch msg.Header.Type {
 		case syscall.NLMSG_DONE:
 			break msgLoop
 		case syscall.RTM_NEWROUTE:
-			route, err := syscall.ParseNetlinkRouteAttr(&msg)
+			route, err := parseRTMNewRoute(&msg)
 			if err != nil {
 				return nil, err
 			}
-			e, err := routeToTableEntry(route)
-			if err != nil {
-				return nil, err
-			}
-			rtTable = append(rtTable, e)
+			rtTable = append(rtTable, route)
 		default:
 			// WTF?  This should never happen, so silently ignore it and pray
 			// that we get something sensible.
@@ -92,10 +106,9 @@ msgLoop:
 	// do this as we go instead of waiting till the entire table has been
 	// parsed, but that doesn't save much time, and this is easier to debug.
 	for _, e := range rtTable {
-		if e.dstAddr == nil && e.gwAddr != nil {
-			return e.gwAddr, nil
+		if e.DstNet.IP == nil && e.GwAddr != nil {
+			return e.GwAddr, nil
 		}
 	}
-
 	return nil, fmt.Errorf("failed to find default gateway")
 }
